00a903e20dfbe682672e63b170f3b4c038a7d39b
toting <yoonzz123@gmail.com>
2023-06-15 21:17:33 +0900

    test commit

---
+++ src/main/java/com/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+    @RequestMapping("/")
+    public String home1(){
+        return "home";
+    }
+
+}
